e463187685b0c2c56aabcd18302c8c5c88aebf98
xu-ji <xj1112@ic.ac.uk>
2013-01-06 01:04:07 +0000

    d

--- jan3.hs
+++ jan3.hs
@@ -1,3 +1,7 @@
+import Data.List
+import Data.Char
+import Data.Maybe
+
 type Program = [ Statement ]
 
 type Id = String
@@ -101,30 +105,118 @@ p5 = [ Assign "x" ( Var "in1" ),
 
 ---------------------------------------------------------------------
 -- Part I
-
+--pre: state always contains binding for Id, and only one instance.
 lookUp :: Id -> State -> Int
+lookUp x states = maybe (error "Not found") id (lookup x states)
 
 update :: ( Id, Int ) -> State -> State
+update pair [] = [pair]
+update (i, val) ((s1, sval):state)
+  | i == s1 = (i, val):state
+  | otherwise = (s1, sval): (update (i, val) state)
 
+--can't pattern match on the exp!
+--uses a simple stack
 assignments :: Program -> Int
+assignments program = assign' program 0
+  where
+  assign' :: Program -> Int -> Int
+  assign' ((Assign i e):pro) n   
+    = assign' pro (n + 1)
+  assign' ((Cond e p1 p2):pro) n 
+    = assign' pro (n + (assign' p1 0) + (assign' p2 0))
+  assign' ((While e p1):pro) n 
+    = assign' pro (n + (assign' p1 0)) 
 
-count :: Statement -> Int
+-- you cannot use guards on pattern matching data types.
+assignments2 :: Program -> Int
+assignments2 program = sum (map count program)
+  where
+  count :: Statement -> Int
+  count (Assign i a)   = 1
+  count (Cond e p1 p2) = (assignments2 p1) + (assignments2 p2)
+  count (While e p1)   = assignments2 p1
+  
 
 ---------------------------------------------------------------------
 -- Part II
 
 apply :: Op -> Int -> Int -> Int
+apply op a b = (fromJust (lookup op opTable)) a b
+ 
+--in lookup tables for functions, always put brackets around.
+opTable :: [(Op, (Int -> Int -> Int))]
+opTable = [(Add, (+)),
+           (Mult, (*)),
+           (Equal, (equals)),
+           (Greater, (greater))]
 
+equals :: Int -> Int -> Int
+equals a b
+  | a == b = 1
+  | otherwise = 0
+
+greater :: Int -> Int -> Int
+greater a b
+  | a > b = 1
+  | otherwise = 0
+
+--data Exp = Const Int | Var Id | BinOp Op Exp Exp
 eval :: Exp -> State -> Int
-
+eval (Const i) _ = i
+eval (Var i) state = lookUp i state 
+eval (BinOp op e1 e2) state = apply op (eval e1 state) (eval e2 state) 
+{-
+data Statement = Assign Id Exp |
+                 Cond Exp Program Program |
+                 While Exp Program
+                 deriving ( Eq, Ord, Show )
+type State = [ ( Id, Int ) ]
+-}
 executeStatement :: Statement -> State -> State
+executeStatement (Assign i e) state = update (id, val) state
+  where (id, val) = (i, (eval e state))
+executeStatement (Cond e pro1 pro2) state
+  | eval e state == 1 = execute pro1 state
+  | otherwise         = execute pro2 state
+executeStatement (While e pro1) state
+  | eval e state == 1 = executeStatement (While e pro1) (execute pro1 state)
+  | otherwise         = state
 
+--execute must perform program actions one at a time + reflect state
 execute :: Program -> State -> State
-
+execute [] state = state
+execute (p:program) state = execute program (executeStatement p state)  
 ---------------------------------------------------------------------
 -- Part III
 
 type Graph = [ ( Int, [ Int ] ) ]
+{-
+graph :: Program -> Graph
+graph (p:program) = graph' (p:program) 1
+  where
+  graph' :: Program -> Int -> Graph
+  graph' [] n = [(n, [0])]
+  graph' ((Assign i exp):pro) n = (n, [n+1]): (graph' program (n+1))
+  graph' ((Cond exp p1 p2):pro) n = (n, n2@(map fst (graph' p1 n+1))++(map fst (graph' p2 n3@((maximum n2)+1)))): (graph' program ((maximum n3)+1)) 
+  graph' ((While exp p1):pro) n =  
+-}
 
 graph :: Program -> Graph
+graph p = graph' p 1
+        where
+        graph' :: Program -> Int -> [Int] -> Graph
+        graph' [] n is = if null is 
+                            then [(n, 0)]
+                            else [(n, head is)]
+        graph' ((Assign i exp): pro) n is = (n, [n+1]): graph' pro (n+1) is
+        graph' ((Cond exp p1 p2): pro) n = (n, [(n+1), (deepMax p1 + n + 1)])
+                                            : 
+        graph' ((While exp p1): pro) n = ()
 
+deepMax :: Program -> Int
+deepMax [] = 0
+deepMax ((Assign _ _): statements) = 1 + deepMax statements
+deepMax ((Cond _ p1 p2): statements) = 2 + (deepMax p1) + (deepMax p2)
+                                        + (deepMax statements)
+deepMax ((While _ p1): statements) = 1 + (deepMax p1) + (deepMax statements)
